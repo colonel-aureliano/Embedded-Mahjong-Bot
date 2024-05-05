7de3ebda7a6909cebabb3719af139ab86e9a67d6
RZ <yanny20011102@gmail.com>
2024-05-05 15:46:06 -0400

    onnx model threshold tuning

--- integrated/camera_onnx_infer.py
+++ integrated/camera_onnx_infer.py
@@ -7,17 +7,14 @@ sys.path.append('')
 from tile_classifier.onnx.yolov9 import YOLOv9
 import time
 
-from camera import shoot_on_button
-
 class Infer:
 
 
     def __init__(self):
         self.photo_path = "integrated/shot_image.jpg"
         self.predictions_path = "integrated/predictions.json"
-        self.generate_image = False
 
-    def inferred_detections(self,detector, image_path):
+    def inferred_detections(self,detector, image_path, generate_image=False):
         image = cv2.imread(image_path)
 
         begin = time.time()
@@ -26,7 +23,7 @@ class Infer:
         elapsed = end - begin
         print(f"Prediction time: {elapsed} seconds")
         
-        if (self.generate_image):
+        if (generate_image):
             detector.draw_detections(image, detections=detections)
             # output_path = f"predicted_{image_path}"
             output_path = "predicted.jpg"
@@ -42,9 +39,9 @@ class Infer:
         classes_path = "tile_classifier/onnx/weights/class_labels.yaml"
         h,w = 2464,3280
         # h,w = 80, 800
-        score_threshold = 0.1
-        conf_threshold = 0.4
-        iou_threshold = 0.4
+        score_threshold = 0.05
+        conf_threshold = 0.01
+        iou_threshold = 0.4   # intersection over union threshold
         device = "cpu"
         return YOLOv9(model_path=weights_path,
                         class_mapping_path=classes_path,
@@ -54,12 +51,11 @@ class Infer:
                         iou_threshold=iou_threshold,
                         device=device)
 
-    def shoot_detect_to_json(self,predictions_path,generate_image):
-        self.generate_image = generate_image
+    def shoot_detect_to_json(self,predictions_path,generate_image = False):
         self.prediction_path = predictions_path
-        self.run(["", "-shoot", "-json"])
+        self.run(["", "-shoot", "-json"], generate_image)
 
-    def run(self,args):
+    def run(self,args,generate_image = False):
         if len(args) <= 1 or args[1] not in ["-single", "-shoot-wait", '-shoot']:
             print("Please provide a valid option: -single for single image prediction, -shoot for taking a photo with Pi Camera then do prediction, -shoot-wait for taking a photo with Pi Camera upon input then do prediction.")
             print("Add -json to store output.")
@@ -70,13 +66,17 @@ class Infer:
         if args[1] == "-single":
             pass
         elif args[1] == "-shoot-wait":
+            from camera import shoot_on_button
             shoot_on_button.no_button_shoot(self.photo_path, True)
             print('Shot and saved at '+self.photo_path)
         elif args[1] == "-shoot":
+            from camera import shoot_on_button
             shoot_on_button.no_button_shoot(self.photo_path, False)
             print('Shot and saved at '+self.photo_path)
 
-        pred = self.inferred_detections(detector,self.photo_path)
+        pred : list[dict] = self.inferred_detections(detector, self.photo_path, generate_image or len(args) > 2 and args[2] == '-generate-image')
+        pred.sort(key=lambda d: d['box'][0])
+
         if len(args) > 2 and args[2] == '-json':
             for d in pred:
                 for key, value in d.items():
@@ -90,7 +90,9 @@ class Infer:
                 json.dump(pred, json_file, indent=4)
                 print(f"Predictions stored at {self.predictions_path}")
         else:
-            print(pred)
+            print(f"Found {len(pred)} tiles")
+            for d in pred:
+                print(d)
 
 if __name__ == "__main__":
     infer = Infer()
--- integrated/main.py
+++ integrated/main.py
@@ -2,33 +2,36 @@ from camera_onnx_infer import Infer
 import json
 from player.Player import Player
 
-def play_round(infer : Infer, player : Player, mapping : dict[str, int]):
+def play_round(infer : Infer, player : Player, tile_mapping : dict[str, int], generate_image = False):
   prediction_path = "integrated/predictions.json"
-  infer.shoot_detect_to_json(prediction_path, True)
+  infer.shoot_detect_to_json(prediction_path, generate_image)
 
   # read predictions.json
   with open(prediction_path) as f:
     predictions = json.load(f)
+
+  reverse_tile_mapping = {v: k for k, v in tile_mapping.items()}
 
   names = []
   tiles = []
   for prediction in predictions:
     name = prediction['class_name']
     names.append(name)
-    tiles.append(mapping[name])
+    tiles.append(tile_mapping[name])
 
   if(len(tiles) != 14):
     print(names)
-    for i in range(14-len(tiles)):
+    for _ in range(14-len(tiles)):
       name = input("Add missing tile: ")
       names.append(name)
-      tiles.append(mapping[name])
+      tiles.append(tile_mapping[name])
 
   for tile in tiles:
     player.add_tile(tile)
     
   tile = player.play_tile()
-  print(tile)
+  name = reverse_tile_mapping[tile]
+  print(name)
 
 def main():
   infer = Infer()
@@ -38,6 +41,8 @@ def main():
   with open("integrated/player_tile_mapping.json") as f:
     mapping = json.load(f)
 
-  play_round(infer, player, mapping)
+  generate_image = False
+
+  play_round(infer, player, mapping, generate_image)
 
 main()
