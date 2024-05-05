0fd119fbc9d6dafe5a1ef2acc5e50387fe25f803
RZ fom Pi <yanny20011102@gmail.com>
2024-05-05 12:57:04 -0400

    bug fix

--- camera_onnx_infer.py
+++ camera_onnx_infer.py
@@ -1,5 +1,7 @@
 import sys
 import cv2
+import json
+import numpy as np
 
 from tile_classifier.onnx.yolov9 import YOLOv9
 import time
@@ -7,16 +9,16 @@ import time
 def inferred_detections(detector, image_path):
     image = cv2.imread(image_path)
 
-    # begin = time.time()
+    begin = time.time()
     detections = detector.detect(image)
-    # end = time.time()
-    # elapsed = end - begin
-    # print(f"Time elapsed: {elapsed} seconds")
+    end = time.time()
+    elapsed = end - begin
+    print(f"Time elapsed: {elapsed} seconds")
 
     return detections
 
 def retreive_onnx():
-    weights_path = "tile_classifier/onnx/weights/best_striped_V3_800_gelan-c.pt"
+    weights_path = "tile_classifier/onnx/weights/best_striped_V3_800_gelan-c.onnx"
     classes_path = "tile_classifier/onnx/weights/class_labels.yaml"
     h,w = 2464,3280
     score_threshold = 0.1
@@ -37,18 +39,27 @@ if __name__ == "__main__":
     detector = retreive_onnx()
     if len(sys.argv) > 1:
         if sys.argv[1] == "-single":
-            pred = inferred_detections(detector,sys.argv[2])
-            print(pred)
-            # with open("predictions.txt", "w") as json_file:
-            #     json.dump(pred, json_file, indent=4) 
+            pred = inferred_detections(detector,"shot_image.jpg")
+            # print(pred)
         elif sys.argv[1] == "-shoot":
             from camera import shoot_on_button
             shoot_on_button.no_button_shoot("shot_image")
             pred = inferred_detections(detector, "shot_image.jpg")
-            print(pred)
-            # with open("predictions.txt", "w") as json_file:
-            #     json.dump(pred, json_file, indent=4)
-
+            # print(pred)
+        else: 
+            print("Please provide a valid option: -single for single image prediction, -shoot for take photo with Pi Camera then do prediction.")
+            sys.exit(1)
+        if len(sys.argv) > 2 and sys.argv[2] == '-json':
+            for d in pred:
+                for key, value in d.items():
+                    if isinstance(value, np.ndarray):
+                        d[key] = value.tolist()
+                    elif isinstance(value, np.int64):
+                        d[key] = int(value)
+                    elif isinstance(value, np.float32):
+                        d[key] = float(value)
+            with open("predictions.json", "w") as json_file:
+                json.dump(pred, json_file, indent=4)
     else:
-        print("Please provide a valid option: -single for single image prediction.")
+        print("Please provide a valid option: -single for single image prediction, -shoot for take photo with Pi Camera then do prediction.")
         sys.exit(1)
